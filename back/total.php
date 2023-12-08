be4fdabcef41fffccc95af7cbb9ecc43a31afde1
Angie <liya0706s@gmail.com>
2023-12-08 09:23:35 +0800

    完成進站總人數後台更新功能

--- back/total.php
+++ back/total.php
@@ -1,6 +1,6 @@
 <div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
     <p class="t cent botli">進站總人數管理</p>
-    <form method="post" target="back" action="?do=tii">
+    <form method="post" action="/api/edit_total.php">
         <table style="width:50%;margin:auto">
             <tbody>
                 <tr class="yel">
@@ -12,7 +12,7 @@
         <table style="margin-top:40px; width:70%;">
             <tbody>
                 <tr>
-                    <td width="200px"><input type="button" onclick="op(&#39;#cover&#39;,&#39;#cvr&#39;,&#39;view.php?do=title&#39;)" value="新增網站標題圖片"></td>
+                    <td width="200px">
                     <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
                 </tr>
             </tbody>
