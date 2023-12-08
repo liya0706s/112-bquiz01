9d1f4db34924b76b0ff532ba9053e716907029cf
Angie <liya0706s@gmail.com>
2023-12-08 08:51:09 +0800

    feat:進站總人數管理

--- back/title copy.php
+++ back/title copy.php
@@ -1,12 +1,14 @@
 <div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
-    <p class="t cent botli">網站標題管理</p>
+    <p class="t cent botli">進站總人數管理</p>
     <form method="post" target="back" action="?do=tii">
         <table width="100%">
             <tbody>
                 <tr class="yel">
                     <td width="45%">網站標題</td>
-                    <td width="23%"><input type="number" name="total" value=""></td>
-                    
+                    <td width="23%">替代文字</td>
+                    <td width="7%">顯示</td>
+                    <td width="7%">刪除</td>
+                    <td></td>
                 </tr>
             </tbody>
         </table>
