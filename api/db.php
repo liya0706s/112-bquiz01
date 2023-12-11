a4275f60b3a8b667e9ff6c5e3de21c32069ef263
Angie <liya0706s@gmail.com>
2023-12-11 10:04:43 +0800

    複製mvim.php成為image.php並完成校園映像資料的後台功能

--- api/db.php
+++ api/db.php
@@ -156,6 +156,8 @@ $Bottom=new DB('bottom');
 $Ad=new DB('ad');
 // 多一張資料表的物件變數
 $Mvim=new DB('mvim');
+$Image=new DB('image');
+
 
 
 
--- back/image.php
+++ back/image.php
@@ -1,22 +1,48 @@
 <div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
-    <p class="t cent botli">校園映像圖片管理</p>
-    <form method="post" target="back" action="?do=tii">
-        <table width="100%">
+    <p class="t cent botli">校園映像資料管理</p>
+    <form method="post" action="./api/edit.php">
+        <!-- edit編輯的欄位是否醫治 -->
+        <table width="100%" style="text-align: center">
             <tbody>
                 <tr class="yel">
-                    <td width="45%">網站標題</td>
-                    <td width="23%">替代文字</td>
-                    <td width="7%">顯示</td>
-                    <td width="7%">刪除</td>
+                    <td width="70%">校園映像資料圖片</td>
+                    <td width="10%">顯示</td>
+                    <td width="10%">刪除</td>
                     <td></td>
                 </tr>
+                <?php
+                // 後台:用foreach迴圈將all()全部的資料倒出來
+                // 前台:才要加條件sh=1的才要
+                $rows = $Image->all();
+                foreach ($rows as $row) {
+                ?>
+                    <tr>
+                        <td>
+                            <img src="./img/<?= $row['img']; ?>" style="width:150px;height:103px">
+                        </td>
+                            <input type="hidden" name="id[]" value="<?=$row['id'];?>">
+                        <td>
+                            <input type="checkbox" name="sh[]" value="<?=$row['id'];?>"<?=($row['sh']==1)?'checked':'';?>>
+                        </td>
+                        <td>
+                            <input type="checkbox" name="del[]" value="<?= $row['id']; ?>">
+                        </td>
+                        <td>
+                            <input type="button" onclick="op('#cover','#cvr','./modal/upload.php?table=<?=$do;?>&id=<?=$row['id'];?>')" value="更換圖片">
+                            <!-- id為了撈資料 -->
+                        </td>
+                    </tr>
+                <?php
+                }
+                ?>
             </tbody>
         </table>
         <table style="margin-top:40px; width:70%;">
             <tbody>
                 <tr>
-                    <input type="hidden" name="table" value="<?=$do;?>">
-                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','view.php?do=title')" value="新增網站標題圖片"></td>
+                    <input type="hidden" name="table" value="<?= $do; ?>">
+                    <!-- 隱藏欄位的意義???? -->
+                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','./modal/<?= $do; ?>.php?table=<?= $do; ?>')" value="新增校園映像圖片"></td>
                     <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
                 </tr>
             </tbody>
