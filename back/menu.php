6a323b77d069c1eccc1f15676c322d9a879c4349
Angie <liya0706s@gmail.com>
2023-12-12 23:14:06 +0800

    改menu彈跳視窗送出按鈕的value

--- back/menu.php
+++ back/menu.php
@@ -44,7 +44,7 @@
             <tbody>
                 <tr>
                     <input type="hidden" name="table" value="<?= $do; ?>">
-                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','./modal/<?= $do; ?>.php?table=<?= $do; ?>')" value="新增管理者帳號"></td>
+                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','./modal/<?= $do; ?>.php?table=<?= $do; ?>')" value="新增主選單"></td>
                     <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
                 </tr>
             </tbody>
