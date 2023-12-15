d8ce7ce6ea945d3579f0fd68136a5a47d34e3bc8
Angie <liya0706s@gmail.com>
2023-12-15 16:32:10 +0800

    css a tag

--- front/news.php
+++ front/news.php
@@ -47,6 +47,7 @@
 		}
 		?>
 	</div>
+	
 	<!-- 插入back.php div和script標籤 hover功能 -->
 	<div id="alt" style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;"></div>
 			<script>
