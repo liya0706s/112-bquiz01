017cf30e1b6585f5e35160e4458d99e9c884d2f5
Angie <liya0706s@gmail.com>
2023-12-15 16:14:48 +0800

    front news管理登入 校園映像區跑掉了

--- front/main.php
+++ front/main.php
@@ -17,7 +17,7 @@
         <?php
         // 有被設為顯示的撈出來，放入空陣列
         // 要把陣列轉成字串寫在js上
-        $lins = $Mvim->all(['sh'=>1]);
+        $lins = $Mvim->all(['sh' => 1]);
         foreach ($lins as $lin) {
             echo "lin.push('{$lin['img']}');";
         }
@@ -50,24 +50,42 @@
 
     <div style="width:95%; padding:2px; height:190px; margin-top:10px; padding:5px 10px 5px 10px; border:#0C3 dashed 3px; position:relative;">
         <span class="t botli">最新消息區
+            <?php
+            if ($News->count(['sh' => 1]) > 5) {
+                echo "<a href='?do=news' style='float:right'>More...</a>";
+            }
+            ?>
         </span>
         <ul class="ssaa" style="list-style-type:circle;">
-
-        <?php
-        // limit前面要空白 sql語法有間隔 limit 5 只有前五筆
-        $news=$News->all(['sh'=>1], ' limit 5');
-            foreach($news as $n){
+            <?php
+            // limit前面要空白 sql語法有間隔 limit 5 只有前五筆
+            $news = $News->all(['sh' => 1], ' limit 5');
+            foreach ($news as $n) {
                 echo "<li>";
-                echo mb_substr($n['text'],0,20);
+                echo mb_substr($n['text'], 0, 20);
+
+                echo "<div class='all' style='display:none'>";
+                echo $n['text'];
+                echo "</div>";
+                
                 echo "...</li>";
+                
             }
-        ?>
+            ?>
 
         </ul>
-        <div id="altt" style="position: absolute; width: 350px; min-height: 100px; background-color: rgb(255, 255, 204); top: 50px; left: 130px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;"></div>
+        <!-- div容器會在下方 指稱id 的發生效用 -->
+        <div id="altt" style="position: absolute; width: 350px; min-height: 100px; background-color: rgb(255, 255, 204); top: 50px; left: 130px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;">
+            <!-- html("<pre>" + $(this).children(".all").html() + "</pre>")-->
+        </div>
         <script>
+            // hover事件的時候，會有function()
             $(".ssaa li").hover(
                 function() {
+                    // html這個方法 pre標籤放到altt的div容器裡面
+                    // jQuery $(this)這個 hover事件發生當下的(會變的) 
+                    // hover過來所有的li的children下一層div裡面有class=all 顯示全部的內容
+                    // all()括號沒有參數:抓全部, 所以html()沒有參數是要這容器的全部內容
                     $("#altt").html("<pre>" + $(this).children(".all").html() + "</pre>")
                     $("#altt").show()
                 }
