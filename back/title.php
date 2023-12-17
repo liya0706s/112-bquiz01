75c0a34f6a8d89a3b08e4944ef9f7eea71627aea
Angie <liya0706s@gmail.com>
2023-12-17 10:48:58 +0800

    feat:add notes on back/title input:hidden

--- back/title.php
+++ back/title.php
@@ -15,7 +15,7 @@
                 <!-- 資料 -->
                 <?php
                 // 後臺管理，意味者管理"全部"的資料 all
-                // 後台:用foreach迴圈將 all()全部的資料撈出來放在$rows裡面
+                // 後台:用foreach迴圈將 all()全部的資料撈出來放在$rows陣列裡面$row是裡面的元素
                 // 前台:才要加條件sh=1的才要
                 // $DB=${ucfirst($do)}; 統一寫在db.php裡了
 
@@ -30,6 +30,7 @@
                         <td width="23%">
                             <input type="text" name="text[]" style="width:90%" value="<?= $row['text']; ?>">
                             <input type="hidden" name="id[]" value="<?= $row['id']; ?>">
+                            <!-- 有input:hidden-id是要傳遞給 api/edit  -->
                         </td>
                         <td width="7%">
                             <input type="radio" name="sh" value="<?= $row['id']; ?>" <?= ($row['sh'] == 1) ? 'checked' : ''; ?>>
@@ -39,7 +40,7 @@
                         </td>
                         <td>
                             <input type="button" onclick="op('#cover','#cvr','./modal/upload.php?table=<?= $do; ?>&id=<?= $row['id']; ?>')" value="更新圖片">
-                            <!-- 有table也有id，知道是要"更新"哪一筆資料 -->
+                            <!-- 有table($do)也有id{$row['id']}，知道是要"更新"哪一筆資料 -->
                         </td>
                     </tr>
                 <?php
@@ -51,6 +52,7 @@
             <tbody>
                 <tr>
                     <input type="hidden" name="table" value="<?= $do; ?>">
+                    <!-- 放input:hidden，來傳遞table -->
                     <td width="200px"><input type="button" onclick="op('#cover','#cvr','./modal/<?= $do; ?>.php?table=<?= $do; ?>')" value="新增網站標題圖片"></td>
                     <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
                 </tr>
